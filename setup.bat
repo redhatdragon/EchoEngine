0ec691ee79d7b1d9a68e1b49efb7f855144b56ad
redhatdragon <paulabrams99@yahoo.com>
2022-10-24 07:03:46 -0400

    Fixed setup.bat

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 mkdir deps
 cd deps
 git clone https://github.com/libsdl-org/SDL SDL2
-https://github.com/libsdl-org/SDL_image SDL2_image
+git clone https://github.com/libsdl-org/SDL_image SDL2_image
 git clone https://github.com/libsdl-org/SDL_ttf SDL2_ttf
 cd SDL2_ttf/external
 git clone https://github.com/freetype/freetype freetype
